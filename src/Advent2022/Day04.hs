6299e1fbc2a003dffaa2fb09b5e59487fec6334a
Chris LaRose <cjlarose@gmail.com>
2022-12-05 21:43:10 -0800

    Complete day 4, part 1

---
+++ src/Advent2022/Day04.hs
@@ -0,0 +1,37 @@
+module Advent2022.Day04
+  ( solve,
+  )
+where
+
+import Advent.Input (getProblemInputAsText)
+import Advent.Parse (Parser, parse)
+import Advent.PuzzleAnswerPair (PuzzleAnswerPair (..))
+import Advent.CommonParsers (linesOf)
+import Data.List (sort, intersect)
+import Data.Char (isLower, ord)
+import Text.Megaparsec (some)
+import Text.Megaparsec.Char (char, digitChar)
+
+type SectionAssignment = (Int, Int)
+type AssignmentPair = (SectionAssignment, SectionAssignment)
+
+inputParser :: Parser [AssignmentPair]
+inputParser = linesOf assignmentPair
+  where
+    assignmentPair = (,) <$> sectionAssignment <* char ',' <*> sectionAssignment
+    sectionAssignment = (,) <$> section <* char '-' <*> section
+    section = read <$> some digitChar
+
+numSubsets :: [AssignmentPair] -> Int
+numSubsets = length . filter hasSubset
+  where
+    hasSubset ((a, b), (x, y)) = x >= a && y <= b || a >= x && b <= y
+
+printResults :: [AssignmentPair] -> PuzzleAnswerPair
+printResults sacks = PuzzleAnswerPair (part1, part2)
+  where
+    part1 = show . numSubsets $ sacks
+    part2 = ""
+
+solve :: IO (Either String PuzzleAnswerPair)
+solve = parse inputParser printResults <$> getProblemInputAsText 4
--- src/Advent2022/Solve.hs
+++ src/Advent2022/Solve.hs
@@ -7,9 +7,11 @@ import Advent.PuzzleAnswerPair (PuzzleAnswerPair (..))
 import qualified Advent2022.Day01
 import qualified Advent2022.Day02
 import qualified Advent2022.Day03
+import qualified Advent2022.Day04
 
 solverForProblem :: Int -> IO (Either String PuzzleAnswerPair)
 solverForProblem 1 = Advent2022.Day01.solve
 solverForProblem 2 = Advent2022.Day02.solve
 solverForProblem 3 = Advent2022.Day03.solve
+solverForProblem 4 = Advent2022.Day04.solve
 solverForProblem n = pure . Left $ "Unknown problem " ++ show n
