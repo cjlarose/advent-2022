b0c9a4ee9185ff654f42fa58719191f47bb1783c
Chris LaRose <cjlarose@gmail.com>
2022-12-05 21:16:56 -0800

    Complete day 3, part 1

---
+++ src/Advent2022/Day03.hs
@@ -0,0 +1,40 @@
+module Advent2022.Day03
+  ( solve,
+  )
+where
+
+import Advent.CommonParsers (linesOf)
+import Advent.Input (getProblemInputAsText)
+import Advent.Parse (Parser, parse)
+import Advent.PuzzleAnswerPair (PuzzleAnswerPair (..))
+import Data.List (sort, intersect)
+import Data.Char (isLower, ord)
+import Text.Megaparsec (some)
+import Text.Megaparsec.Char (letterChar)
+
+type ItemPriority = Int
+type Rucksack = ([ItemPriority], [ItemPriority])
+
+inputParser :: Parser [Rucksack]
+inputParser = linesOf rucksack
+
+rucksack :: Parser Rucksack
+rucksack = readCompartments <$> some (priority <$> letterChar)
+  where
+    readCompartments xs = splitAt (length xs `div` 2) xs
+    priority x | isLower x = ord x - ord 'a' + 1
+               | otherwise = ord x - ord 'A' + 27
+
+misplacedItemPrioritySum :: [Rucksack] -> Int
+misplacedItemPrioritySum = sum . map f
+  where
+    f (left, right) = head $ left `intersect` right
+
+printResults :: [Rucksack] -> PuzzleAnswerPair
+printResults sacks = PuzzleAnswerPair (part1, part2)
+  where
+    part1 = show . misplacedItemPrioritySum $ sacks
+    part2 = ""
+
+solve :: IO (Either String PuzzleAnswerPair)
+solve = parse inputParser printResults <$> getProblemInputAsText 3
--- src/Advent2022/Solve.hs
+++ src/Advent2022/Solve.hs
@@ -6,8 +6,10 @@ where
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair (..))
 import qualified Advent2022.Day01
 import qualified Advent2022.Day02
+import qualified Advent2022.Day03
 
 solverForProblem :: Int -> IO (Either String PuzzleAnswerPair)
 solverForProblem 1 = Advent2022.Day01.solve
 solverForProblem 2 = Advent2022.Day02.solve
+solverForProblem 3 = Advent2022.Day03.solve
 solverForProblem n = pure . Left $ "Unknown problem " ++ show n
