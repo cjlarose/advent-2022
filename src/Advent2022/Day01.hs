a64441e0651170634b613e43ee8d19cf6b11040e
Chris LaRose <cjlarose@gmail.com>
2022-12-05 19:04:05 -0800

    Complete day 1 part 2

--- src/Advent2022/Day01.hs
+++ src/Advent2022/Day01.hs
@@ -8,6 +8,7 @@ import Advent.Input (getProblemInputAsText)
 import Advent.Parse (Parser, parse)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair (..))
 import Numeric.Natural (Natural)
+import Data.List (sort)
 import Text.Megaparsec (eof, some, sepBy1)
 import Text.Megaparsec.Char (newline)
 
@@ -20,11 +21,14 @@ elfInventory = some (natural <* newline)
 greatestCalories :: [[Natural]] -> Natural
 greatestCalories = maximum . map sum
 
+caloriesOfTopThreeElves :: [[Natural]] -> Natural
+caloriesOfTopThreeElves = sum . take 3 . reverse . sort . map sum
+
 printResults :: [[Natural]] -> PuzzleAnswerPair
 printResults elfInventories = PuzzleAnswerPair (part1, part2)
   where
     part1 = show . greatestCalories $ elfInventories
-    part2 = ""
+    part2 = show . caloriesOfTopThreeElves $ elfInventories
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = parse inputParser printResults <$> getProblemInputAsText 1
