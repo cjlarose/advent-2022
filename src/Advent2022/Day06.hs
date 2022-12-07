bef20fcf19bd01835ec6c41a97d75eb5b8bb6b4e
Chris LaRose <cjlarose@gmail.com>
2022-12-07 11:18:59 -0800

    Switch input for integers

--- src/Advent2022/Day06.hs
+++ src/Advent2022/Day06.hs
@@ -6,21 +6,22 @@ where
 import Advent.Input (getProblemInputAsText)
 import Advent.Parse (Parser, parse)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair (..))
+import Data.Char (ord)
 import Text.Megaparsec (some, eof)
 import Text.Megaparsec.Char (lowerChar, newline, eol)
 
-inputParser :: Parser String
-inputParser = some lowerChar <* newline <* eof
+inputParser :: Parser [Int]
+inputParser = map (\c -> ord c - ord 'a') <$> some lowerChar <* newline <* eof
 
-prefixLength :: String -> Int
+prefixLength :: [Int] -> Int
 prefixLength = f 0
   where
-    f :: Int -> String -> Int
+    f :: Int -> [Int] -> Int
     f dropped (a:b:c:d:xs)
       | a /= b && a /= c && a /= c && a /= d && b /= c && b /= d && c /= d = dropped + 4
       | otherwise = f (dropped + 1) $ b:c:d:xs
 
-printResults :: String -> PuzzleAnswerPair
+printResults :: [Int] -> PuzzleAnswerPair
 printResults input = PuzzleAnswerPair (part1, part2)
   where
     part1 = show . prefixLength $ input
