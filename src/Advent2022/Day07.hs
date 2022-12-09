1bb400fc6adaae5d9cb603cb30132c845de1d1a0
Chris LaRose <cjlarose@gmail.com>
2022-12-08 23:01:45 -0800

    Complete day 7 part 1

--- src/Advent2022/Day07.hs
+++ src/Advent2022/Day07.hs
@@ -9,11 +9,14 @@ import Advent.Parse (Parser, parse)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair (..))
 import Advent.CommonParsers (symbol, token)
 import Data.Text (Text, pack)
+import qualified Data.Map as Map
+import Data.Map ((!))
 import Text.Megaparsec (some, eof, (<|>))
 import Text.Megaparsec.Char (lowerChar, newline, eol, string, letterChar, char, digitChar)
 
 data Command = ChangeDirectory Text | DirectoryListing [File]
-data File = Directory Text | RegularFile Int Text
+data File = Directory Text | RegularFile Int Text deriving (Eq, Ord, Show)
+type DirectoryTree = Map.Map [Text] [File]
 
 inputParser :: Parser [Command]
 inputParser = some command <* eof
@@ -35,18 +38,44 @@ inputParser = some command <* eof
     filename :: Parser Text
     filename = pack <$> some (char '.' <|> char '/' <|> letterChar)
 
-    directory = Directory <$> symbol "dir" <* filename
+    directory = Directory <$> (symbol "dir" *> filename)
     filesize :: Parser Int
     filesize = read <$> some digitChar
     regularFile = RegularFile <$> token filesize <*> filename
 
-sumOfSizesOfBigDirectories :: [Command] -> Int
-sumOfSizesOfBigDirectories _ = 0
+buildDirectoryTree :: [Command] -> DirectoryTree
+buildDirectoryTree = f Map.empty []
+  where
+    f tree _ [] = tree
+    f tree path (ChangeDirectory ".." : xs) = f tree (tail path) xs
+    f tree path (ChangeDirectory dir : xs) = f tree (dir : path) xs
+    f tree path (DirectoryListing files : xs) = f (Map.insert path files tree) path xs
+
+allDirectorySizes :: DirectoryTree -> Map.Map [File] Int
+allDirectorySizes tree = sizes [] (Directory "/")
+  where
+    sizes :: [File] -> File -> Map.Map [File] Int
+    sizes parentPath f@(RegularFile size name) = Map.singleton (f: parentPath) size
+    sizes parentPath f@(Directory name) =
+      let
+        children :: [File]
+        children = tree ! (name : map fname parentPath)
+        fname (RegularFile _ n) = n
+        fname (Directory n) = n
+        newKnownSizes = foldl Map.union Map.empty . map (sizes (f : parentPath)) $ children
+        newKnownSizes' = Map.insert (f : parentPath) (sum . map (\x -> newKnownSizes ! (x : f : parentPath)) $ children) newKnownSizes
+      in newKnownSizes'
+
+sumOfSizesOfSmolDirectories :: [Command] -> Int
+sumOfSizesOfSmolDirectories = sum . Map.elems . Map.filter (\x -> x <= 100000) . Map.filterWithKey (\k _ -> isDir k) . allDirectorySizes . buildDirectoryTree
+  where
+    isDir (Directory _ : _) = True
+    isDir _ = False
 
 printResults :: [Command] -> PuzzleAnswerPair
 printResults commands = PuzzleAnswerPair (part1, part2)
   where
-    part1 = show . sumOfSizesOfBigDirectories $ commands
+    part1 = show . sumOfSizesOfSmolDirectories $ commands
     part2 = ""
 
 solve :: IO (Either String PuzzleAnswerPair)
