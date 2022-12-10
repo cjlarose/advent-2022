3912fe79e5aa8b0ff5924d3eb5ab307aca0f6c71
Chris LaRose <cjlarose@gmail.com>
2022-12-09 22:04:51 -0800

    Complete day 10 part 2

--- src/Advent2022/Day10.hs
+++ src/Advent2022/Day10.hs
@@ -40,11 +40,24 @@ signalStrengths keyCycles = f keyCycles . machineStates
         ((c, x) : states') = dropWhile (\(c, _) -> c < keyCycle) states
       in c * x : f xs states'
 
+grid :: [Instruction] -> String
+grid = unlines . rows . pixels . machineStates
+  where
+    rows [] = []
+    rows xs = let (row, rest) = splitAt 40 xs in row : rows rest
+    pixels [] = []
+    pixels ((c, x) : xs) =
+      let
+        y = (c - 1) `mod` 40
+        lit = abs (y - x) <= 1
+        pixel = if lit then '#' else '.'
+      in pixel : pixels xs
+
 printResults :: [Instruction] -> PuzzleAnswerPair
 printResults instructions = PuzzleAnswerPair (part1, part2)
   where
     part1 = show . sum . signalStrengths [20, 60..220] $ instructions
-    part2 = "TODO"
+    part2 = grid $ instructions
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = parse inputParser printResults <$> getProblemInputAsText 10
