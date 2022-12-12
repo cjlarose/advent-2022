40c8dae5f2c4ec0b32c6eba8b179409ec3ed0d38
Chris LaRose <cjlarose@gmail.com>
2022-12-12 00:37:20 -0800

    Complete day 11 part 2

--- src/Advent2022/Day11.hs
+++ src/Advent2022/Day11.hs
@@ -21,12 +21,14 @@ import Advent.CommonParsers (integerWithOptionalLeadingSign, token, symbol)
 data Monkey = Monkey
   { items :: [Int]
   , operation :: Int -> Int
+  , divisor ::  Int
   , recipientMonkey :: Int -> Int -- Item -> Monkey to throw to
   }
 data Operand = Old | ImmediateConstant Int
 data MonkeyGame = MonkeyGame
   { monkeys :: Map.Map Int Monkey
   , numInspections :: Map.Map Int Int -- Monkey ID -> number of inspections
+  , gameDivisor :: Int
   }
 
 inputParser :: Parser [Monkey]
@@ -38,15 +40,15 @@ inputParser = some monkey <* eof
     makeOperation f Old x = f x x
     makeOperation f (ImmediateConstant imm) x = f x imm
     operation = makeOperation <$> (symbol "Operation: new = old" *> token (((*) <$ char '*') <|> (+) <$ char '+')) <*> token operand
-    makeTest divisor mTrue mFalse x = if x `mod` divisor == 0 then mTrue else mFalse
+    makeTest divisor mTrue mFalse = (divisor, \x -> if x `mod` divisor == 0 then mTrue else mFalse)
     ifTrue = symbol "If true: throw to monkey" *> token integerWithOptionalLeadingSign
     ifFalse = symbol "If false: throw to monkey" *> token integerWithOptionalLeadingSign
-    test :: Parser (Int -> Int)
+    test :: Parser (Int, (Int -> Int))
     test = makeTest <$> (symbol "Test: divisible by" *> token integerWithOptionalLeadingSign) <*> ifTrue <*> ifFalse
-    monkey = Monkey <$> (monkeyId *> startingItems) <*> operation <*> test
+    monkey = (\mid op (divisor, f) -> Monkey mid op divisor f) <$> (monkeyId *> startingItems) <*> operation <*> test
 
-simulateRound :: State MonkeyGame ()
-simulateRound = get >>= sequence_ . map simulateMonkey . Map.keys . monkeys
+simulateRound :: (Int -> Int) -> State MonkeyGame ()
+simulateRound updateWorryLevel = get >>= sequence_ . map simulateMonkey . Map.keys . monkeys
   where
     simulateMonkey :: Int -> State MonkeyGame ()
     simulateMonkey mid = whileM_ (monkeyHasItems mid) (inspectNextItem mid)
@@ -89,28 +91,30 @@ simulateRound = get >>= sequence_ . map simulateMonkey . Map.keys . monkeys
     inspectNextItem :: Int -> State MonkeyGame ()
     inspectNextItem mid = do
       item <- popItem mid
-      monkey <- lookupMonkey mid <$> get
+      game <- get
+      let monkey = lookupMonkey mid game
       let afterMonkeyOperation = operation monkey item
       recordInspectionForMonkey mid
-      let newWorryLevel = afterMonkeyOperation `div` 3
+      let newWorryLevel = updateWorryLevel afterMonkeyOperation `mod` gameDivisor game
       let recipient = recipientMonkey monkey newWorryLevel
       sendItemToMonkey recipient newWorryLevel
 
-simulateRounds :: Int -> State MonkeyGame ()
-simulateRounds n = sequence_ (take n . repeat $ simulateRound)
+simulateRounds :: Int -> (Int -> Int) -> State MonkeyGame ()
+simulateRounds n updateWorryLevel = sequence_ (take n . repeat $ simulateRound updateWorryLevel)
 
-monkeyBusiness :: [Monkey] -> Int
-monkeyBusiness monkeys = product . take 2 . topActivityLevels $ finalState
+monkeyBusiness :: Int -> (Int -> Int) -> [Monkey] -> Int
+monkeyBusiness numRounds updateWorryLevel monkeys = product . take 2 . topActivityLevels $ finalState
   where
-    initialState = MonkeyGame (Map.fromList . zip [0..] $ monkeys) Map.empty
-    finalState = execState (simulateRounds 20) initialState
+    d = product . map divisor $ monkeys -- LCM is probably better
+    initialState = MonkeyGame (Map.fromList . zip [0..] $ monkeys) Map.empty d
+    finalState = execState (simulateRounds numRounds updateWorryLevel) initialState
     topActivityLevels = reverse . sort . Map.elems . numInspections
 
 printResults :: [Monkey] -> PuzzleAnswerPair
 printResults monkeys = PuzzleAnswerPair (part1, part2)
   where
-    part1 = show . monkeyBusiness $ monkeys
-    part2 = "TODO"
+    part1 = show . monkeyBusiness 20 (flip div 3) $ monkeys
+    part2 = show . monkeyBusiness 10000 id $ monkeys
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = parse inputParser printResults <$> getProblemInputAsText 11
